6bb661e3d3a49d2f207b33d9d2ad4703718fa996
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-21 15:39:12 +0300

    updated the userfind class

--- src/app/repofind.ts
+++ src/app/repofind.ts
@@ -1,2 +1,5 @@
 export class Repofind {
+  constructor(public name:string,public description:string,public since:string){
+
+  }
 }
--- src/app/userfind.ts
+++ src/app/userfind.ts
@@ -1,2 +1,5 @@
-export class Userfind {
+export class Userfind {  
+  constructor(public name:string,public bio:string,public followers:number,public following:number){
+
+  }
 }
