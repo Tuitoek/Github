018d630dc5418020f31ab0ec7515999c884ec88f
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-21 15:58:02 +0300

    registered the service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {UserService} from 'userservice';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import{UserFind} from '../userfind';
 
 @Injectable({
   providedIn: 'root'
